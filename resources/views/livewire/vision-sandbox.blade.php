e9a5258ae8897ae1e621fe2550f3427cdcb7234a
jurdunnn <jordy.downs@hotmail.co.uk>
2024-01-18 21:23:43 +0000

    Add check to ensure user has transcriptions

--- resources/views/livewire/vision-sandbox.blade.php
+++ resources/views/livewire/vision-sandbox.blade.php
@@ -19,51 +19,53 @@
         </button>
     </form>
 
-    <div class="mt-5">
-        <h1 class="text-3xl font-bold underline">Transcriptions</h1>
-
+    @if (auth()->user()->transcriptions)
         <div class="mt-5">
-            @foreach (auth()->user()->transcriptions as $transcription)
-                <div class="px-4 mb-5">
-                    <div class="flex justify-between">
-                        <div>
-                            <h2 class="font-bold">{{ $transcription->id }} - Tokens: {{ $transcription->tokens }}</h2>
+            <h1 class="text-3xl font-bold underline">Transcriptions</h1>
 
-                            <h3 class="font-bold">{{ $transcription->url }}</h3>
-                        </div>
+            <div class="mt-5">
+                @foreach (auth()->user()->transcriptions as $transcription)
+                    <div class="px-4 mb-5">
+                        <div class="flex justify-between">
+                            <div>
+                                <h2 class="font-bold">{{ $transcription->id }} - Tokens: {{ $transcription->tokens }}</h2>
 
-                        <button class="px-4 py-2 text-white bg-blue-600 shadow-xl border-1 rounded-xl" wire:click="analyse({{ $transcription->id }})">
-                            Analyse
-                        </button>
-                    </div>
-
-                    <div class="flex flex-row gap-x-4 mt-4">
-                        <img src="{{ $transcription->url }}" alt="">
-
-                        <div class="flex flex-col gap-y-8">
-                            <div class="p-4 bg-gray-300 shadow-xl">
-                                <h3 class="font-bold">Transcript</h3>
-
-                                <p>
-                                    {{ $transcription->text }}
-                                </p>
+                                <h3 class="font-bold">{{ $transcription->url }}</h3>
                             </div>
 
-                            @if ($transcription->analysis)
-                                <div class="p-4 bg-gray-300 shadow-xl">
-                                    <h3 class="font-bold">Analysis</h3>
+                            <button class="px-4 py-2 text-white bg-blue-600 shadow-xl border-1 rounded-xl" wire:click="analyse({{ $transcription->id }})">
+                                Analyse
+                            </button>
+                        </div>
 
-                                    @foreach ($transcription->analysis->errors as $error)
-                                        <p>
-                                            {{ $error['error_position'] }}: {{ $error['description'] }}
-                                        </p>
-                                    @endforeach
+                        <div class="flex flex-row gap-x-4 mt-4">
+                            <img src="{{ $transcription->url }}" alt="">
+
+                            <div class="flex flex-col gap-y-8">
+                                <div class="p-4 bg-gray-300 shadow-xl">
+                                    <h3 class="font-bold">Transcript</h3>
+
+                                    <p>
+                                        {{ $transcription->text }}
+                                    </p>
                                 </div>
-                            @endif
+
+                                @if ($transcription->analysis)
+                                    <div class="p-4 bg-gray-300 shadow-xl">
+                                        <h3 class="font-bold">Analysis</h3>
+
+                                        @foreach ($transcription->analysis->errors as $error)
+                                            <p>
+                                                {{ $error['error_position'] }}: {{ $error['description'] }}
+                                            </p>
+                                        @endforeach
+                                    </div>
+                                @endif
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
-    </div>
+    @endif
 </div>
