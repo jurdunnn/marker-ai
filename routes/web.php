d268d6e784647c5866c9015b7f844060836268d4
jurdunnn <jordy.downs@hotmail.co.uk>
2024-01-19 17:28:41 +0000

    Add comments to dashboard and profile routes

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,12 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth')->group(function () {
+    // Dashboard
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->middleware(['verified'])->name('dashboard');
 
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
