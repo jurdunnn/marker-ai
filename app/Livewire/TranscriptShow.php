437124334c5bb2462d250791fc67bd53066dd828
jurdunnn <jordy.downs@hotmail.co.uk>
2024-01-20 12:28:56 +0000

    Complete transcript show page

--- app/Livewire/TranscriptShow.php
+++ app/Livewire/TranscriptShow.php
@@ -2,10 +2,13 @@
 
 namespace App\Livewire;
 
+use App\Models\Transcription;
 use Livewire\Component;
 
 class TranscriptShow extends Component
 {
+    public Transcription $transcript;
+
     public function render()
     {
         return view('livewire.transcript-show')->layout('layouts.app');
--- resources/views/livewire/transcript-show.blade.php
+++ resources/views/livewire/transcript-show.blade.php
@@ -1,3 +1,61 @@
+<x-slot name="header">
+    {{ __('Transcript for ' . $transcript->student->name . ' - ' . $transcript->status->name) }}
+</x-slot>
+
+<x-slot name="headerButtons">
+    <button onclick="location.href='{{ route('transcript.edit', ['transcript' => $transcript->id]) }}'"  class="header-button">
+        {{ __('Edit Transcript') }}
+    </button>
+</x-slot>
+
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <div>
+        <x-container>
+            <h2 class="text-xl font-semibold">{{ __('Transcript Information') }}</h2>
+
+            <table class="w-full">
+                <tbody>
+                    <tr>
+                        <td class="py-4 text-sm whitespace-nowrap">{{ __('URL') }}</td>
+                        <td class="py-4 text-sm whitespace-nowrap">{{ $transcript->url }}</td>
+                    </tr>
+                    <tr>
+                        <td class="py-4 text-sm whitespace-nowrap">{{ __('Student') }}</td>
+                        <td class="py-4 text-sm whitespace-nowrap">{{ $transcript->student->name }}</td>
+                    </tr>
+
+                    <tr>
+                        <td class="py-4 text-sm whitespace-nowrap">{{ __('Subject') }}</td>
+                        <td class="py-4 text-sm whitespace-nowrap">{{ $transcript->subject->name }}</td>
+                    </tr>
+
+                    <tr>
+                        <td class="py-4 text-sm whitespace-nowrap">{{ __('Status') }}</td>
+                        <td class="py-4 text-sm whitespace-nowrap">{{ $transcript->status->name }}</td>
+                    </tr>
+
+                    <tr>
+                        <td class="py-4 text-sm whitespace-nowrap">{{ __('Exam') }}</td>
+                        <td class="py-4 text-sm whitespace-nowrap">{{ $transcript->exam->name }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </x-container>
+    </div>
+
+    <div class="mt-8">
+        <x-image-and-container image="{{ $transcript->url }}">
+            @if ($transcript->text)
+                <div class="prose">
+                    {!! $transcript->text !!}
+                </div>
+            @else
+                <div class="p-6 text-gray-900">
+                    {{ __('No transcript text available.') }}
+                </div>
+            @endif
+
+        </x-image-and-container>
+    </div>
 </div>
+
