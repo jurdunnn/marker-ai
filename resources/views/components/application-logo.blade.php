0898cd4789ca2051704b6c5db38d9c152a8b9243
jurdunnn <jordy.downs@hotmail.co.uk>
2024-03-14 16:56:16 +0000

    Add size attribute to application logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,6 @@
-<div class="hover:scale-[153%] scale-[150%] flex -gap-x-1 hover:font-bold" {{ $attributes }}>
+@props(['attributes' => [], 'size' => 150])
+
+<div class="hover:scale-[{{ $size + 3 }}%] scale-[{{ $size }}%] flex -gap-x-1 hover:font-bold" {{ $attributes }}>
     <span class="text-4xl font-bold text-blue-600">
         M
     </span>
