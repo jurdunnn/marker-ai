202b2c3ddd58e27530cad13e3db4846c92ab68f4
jurdunnn <jordy.downs@hotmail.co.uk>
2024-03-17 17:16:30 +0000

    Adjust nav menu icons

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,35 +12,34 @@
 
                 <!-- Navigation Links -->
                 <div class="flex flex-col justify-center px-4 py-8 mt-12 gap-y-5">
-                    <x-nav-link icon="gauge" :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link icon="table-columns" :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
 
-                    <x-nav-link icon="robot" :href="route('marker-bot')" :active="request()->routeIs('marker-bot')">
+                    <x-nav-link icon="brain" :href="route('marker-bot')" :active="request()->routeIs('marker-bot')">
                         {{ __('Marker AI') }}
                     </x-nav-link>
 
-                    <x-nav-link icon="user" :href="route('student.index')" :active="request()->routeIs('student.*')">
+                    <x-nav-link icon="user-group" :href="route('student.index')" :active="request()->routeIs('student.*')">
                         {{ __('Students') }}
                     </x-nav-link>
 
-                    <x-nav-link icon="school" :href="route('exam.index')" :active="request()->routeIs('exam.*')">
+                    <x-nav-link icon="file-lines" :href="route('exam.index')" :active="request()->routeIs('exam.*')">
                         {{ __('Exams') }}
                     </x-nav-link>
 
-                    <x-nav-link icon="flask" :href="route('subject.index')" :active="request()->routeIs('subject.*')">
+                    <x-nav-link icon="book" :href="route('subject.index')" :active="request()->routeIs('subject.*')">
                         {{ __('Subjects') }}
                     </x-nav-link>
 
-                    <x-nav-link icon="file" :href="route('transcript.index')" :active="request()->routeIs('transcript.*')">
+                    <x-nav-link icon="images" :href="route('transcript.index')" :active="request()->routeIs('transcript.*')">
                         {{ __('Transcripts') }}
                     </x-nav-link>
                 </div>
             </div>
 
-            <!-- Settings Dropdown -->
             <button onclick="location.href='{{ route('profile.edit') }}'" class="flex flex-row items-center p-1 mb-8 hover:border-2 hover:shadow-xl hover:rounded-lg hover:border-gray-400 gap-x-4">
-                <div class="flex items-center justify-center my-auto rounded-full bg-slate-400 size-12">
+                <div class="flex items-center justify-center my-auto rounded-full bg-primary size-12">
                     <span class="w-8 text-center text-white group-hover:block">
                         <i class="fa-solid fa-user fa-xl"></i>
                     </span>
